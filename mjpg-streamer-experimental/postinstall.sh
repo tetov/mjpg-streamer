1519b5ddafb7714941956794319191b351ebd738
Anton Tetov <anton@tetov.xyz>
2021-12-11 18:11:53 +0000

    arg wrong after change to useradd

--- mjpg-streamer-experimental/postinstall.sh
+++ mjpg-streamer-experimental/postinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-useradd --system --ingroup video --base-dir /var/lib --create-home --shell /usr/sbin/nologin mjpg_streamer
+useradd --system --groups video --base-dir /var/lib --create-home --shell /usr/sbin/nologin mjpg_streamer
 mkdir -p /var/lib/mjpeg_streamer/history
