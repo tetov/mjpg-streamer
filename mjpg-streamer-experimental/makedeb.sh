6784df54c8aae8e93cf14a9abd5e447ffe1e2fb1
Anton Tetov <anton@tetov.se>
2023-04-10 11:34:35 +0200

    fix permissions on files in deb package

--- mjpg-streamer-experimental/makedeb.sh
+++ mjpg-streamer-experimental/makedeb.sh
@@ -2,10 +2,21 @@
 
 rm -rf _pkg
 install -Dm755 mjpg_streamer _pkg/usr/bin/mjpg_streamer
-install -d _pkg/usr/lib
-install *.so _pkg/usr/lib
-install -D mjpg_streamer@.service _pkg/lib/systemd/system/mjpg_streamer@.service
+install -Dm755 ./*.so _pkg/usr/lib
+install -Dm644 mjpg_streamer@.service _pkg/lib/systemd/system/mjpg_streamer@.service
 mkdir -p _pkg/usr/share/mjpg_streamer
 cp -r www _pkg/usr/share/mjpg_streamer/www
 
-fpm --output-type deb --input-type dir --chdir _pkg --after-install postinstall.sh --name mjpg-streamer --version $VERSION --iteration $REVISION --deb-compression xz --url https://github.com/tetov/mjpg_streamer --description "mjpg-streamer is a command line application that copies JPEG frames from one or more input plugins to multiple output plugins." --maintainer "Anton Tetov <anton@tetov.se>" --license GPLv2
+fpm --output-type deb \
+    --input-type dir \
+    --chdir _pkg \
+    --after-install postinstall.sh \
+    --name mjpg-streamer \
+    --version "$VERSION" \
+    --iteration "$REVISION" \
+    --deb-compression xz \
+    --url https://github.com/tetov/mjpg-streamer \
+    --description "mjpg-streamer is a command line application that copies JPEG frames from one or more input plugins to multiple output plugins." \
+    --maintainer "Anton Tetov <anton@tetov.se>" \
+    --depends libjpeg62-turbo \
+    --license GPLv2
